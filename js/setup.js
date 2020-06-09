7c05136a0f66b5c748becd4e9fbbed9c6104a461
alext <softlink@bk.ru>
2020-06-09 10:59:08 +0700

    исправления

--- js/setup.js
+++ js/setup.js
@@ -4,55 +4,69 @@ var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
 var similarListElement = userDialog.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+.content
+.querySelector('.setup-similar-item');
 
-var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var names = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+
+var surnames = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг',
+];
+
+var coatColors = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)',
+];
+
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
 
-var getRandomValue = function (inputValue) {
+var getRandomElement = function (arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
 
-  for (var i = 0; i < inputValue.length; i++) {
-    var length = inputValue.length;
-    var randomValue = inputValue[Math.floor(Math.random() * (length - 0)) + 0];
-  }
-
-  return randomValue;
+  return arr[randomIndex];
 };
 
 var getRandomName = function (name, surname) {
 
-  var tempName = getRandomValue(name);
-  var tempSurname = getRandomValue(surname);
+  var tempName = getRandomElement(name);
+  var tempSurname = getRandomElement(surname);
   var wizardName = tempName + ' ' + tempSurname;
   return wizardName;
 
-};
-
-var getRandomColor = function (color) {
-
-  color = getRandomValue(color);
-
-  return color;
-};
-
-var createObjectWizard = function (name, surname, coat, eye) {
-
-  var wizard = {};
-  wizard.name = getRandomName(name, surname);
-  wizard.coatColor = getRandomColor(coat);
-  wizard.eyeColor = getRandomColor(eye);
-
-  return wizard;
 };
 
 var createArrayWizards = function (countWizards, name, surname, coat, eye) {
 
   var wizards = [];
   for (var i = 0; i < countWizards; i++) {
-    wizards.push(createObjectWizard(name, surname, coat, eye));
+    var wizard = {
+      name: getRandomName(name, surname),
+      coatColor: getRandomElement(coat),
+      eyeColor: getRandomElement(eye)
+    }
+    wizards.push(wizard);
   }
 
   return wizards;
@@ -60,12 +74,12 @@ var createArrayWizards = function (countWizards, name, surname, coat, eye) {
 
 var wizards = createArrayWizards(4, names, surnames, coatColors, eyesColors);
 
-var renderWizard = function (wizard) {
+var renderWizard = function (template, wizard) {
 
-  var wizardElement = similarWizardTemplate.cloneNode(true);
+  var wizardElement = template.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyeColor;
 
   return wizardElement;
 };
@@ -73,8 +87,9 @@ var renderWizard = function (wizard) {
 var fragment = document.createDocumentFragment();
 
 for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
+  fragment.appendChild(renderWizard(similarWizardTemplate, wizards[i]));
 }
+
 similarListElement.appendChild(fragment);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
